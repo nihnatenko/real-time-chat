3e58d5a3ccb45f58ef39a1ea94117fdefb02e155
nilihnatenko <nihnatenko@wiley.com>
2025-07-02 20:44:13 +0300

    Configure for production

--- backend/src/appModule.ts
+++ backend/src/appModule.ts
@@ -3,16 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatModule } from './chat/chatModule';
 import { MessageModule } from './message/messageModule';
 import { Message } from './message/messageEntity';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.prod' });
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
             type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: '-',
-            password: '-',
-            database: 'chat_db',
+            host: process.env.DB_HOST || 'localhost',
+            port: Number(process.env.DB_PORT) || 3306,
+            username: process.env.DB_USERNAME || '-',
+            password: process.env.DB_PASSWORD || '-',
+            database: process.env.DB_NAME || 'chat_db',
             entities: [Message],
             synchronize: true,
         }),
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './appModule';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+    dotenv.config({ path: '.env' });
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:63342',
+        origin: '*',
         methods: ['GET', 'POST'],
     });
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3000);
+    console.log('🚀 Server is running on http://localhost:3000');
 }
 bootstrap();
